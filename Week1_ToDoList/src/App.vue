336773207b0f519a784a8b94cff3f6f4e3e18740
JoanneWang <jlts713@gmail.com>
2018-06-06 00:20:49 +0800

    Init TodoList
    1. Add stylesheets
    2. Finish menu header

---
+++ Week1_ToDoList/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <to-do-list></to-do-list>
+  </div>
+</template>
+
+<script>
+import ToDoList from './components/ToDolist';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Week1_ToDoList/src/components/ToDoList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="page-header">
+    <div v-for="page in menuPages" :key="page.name"
+      class="menu"
+      :class="{'selected-page': selectedPage === page.name}"
+      @click="setSelectedPage(page.name)">
+      {{ page.name }}
+    </div>
+  </div>
+</template>
+<script>
+export default {
+   data(){
+     return {
+       selectedPage: 'My Tasks',
+       menuPages: [
+        {
+          name: 'My Tasks',
+        },
+        {
+          name: 'In Progress',
+        },
+        {
+          name: 'Completed',
+        }
+       ],
+     };
+   },
+   methods: {
+     setSelectedPage(pagename) {
+       this.selectedPage = pagename; 
+     },
+   },
+}
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/variables.scss';
+
+$header-height: 76px;
+#page-header {
+  @include font-title();
+  width: 100%;
+  height: $header-height;
+  background: $color-light-blue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .menu {
+    box-sizing: border-box;
+    color: $color-dark-blue;
+    flex: 0 0 220px;
+    height: $header-height;
+    line-height: $header-height;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      color: $color-white;
+    }
+    &.selected-page {
+      color: $color-white;
+      border-bottom: 5px solid $color-dark-blue;
+    }
+  }
+}
+
+</style>
